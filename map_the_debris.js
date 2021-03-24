01a057f2f6c1cd4d92b0ebea2ce2b0039ca3cfda
Anton Dovnar <fode4cun@gmail.com>
2021-03-24 20:46:59 +0300

    Mat the debris

---
+++ map_the_debris.js
@@ -0,0 +1,13 @@
+function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    arr.forEach(function(item) {
+        item.orbitalPeriod = Math.round(
+            2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM)
+        );
+        delete item.avgAlt;
+    });
+    return arr;
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
