3a664e5de153b3ea9a5380eaa0810a493de1c990
Anton Dovnar <fode4cun@gmail.com>
2021-07-19 18:46:16 +0300

    URLify

---
+++ challenges/urlify.js
@@ -0,0 +1,7 @@
+const urlify = (blogTitle) => {
+    pattern = /\W+/g;
+    blogTitle = blogTitle.toLowerCase().replace(pattern, '-');
+    return blogTitle;
+};
+
+console.log(urlify("Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking"));
